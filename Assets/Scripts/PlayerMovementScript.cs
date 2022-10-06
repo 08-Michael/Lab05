1c22882b70eb7e0ae9eee2c7a385f1e70d6ad637
08-Michael <michaelbrentdoransegaya@gmail.com>
2022-10-06 11:34:04 +0800

    Updated player script
    Updated script for coin collision

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+    public GameObject CoinCountUI;
+
+    int CoinCount;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,22 @@ public class PlayerMovementScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            CoinCount++;
+
+            CoinCountUI.GetComponent<Text>().text = "CoinCollected: " + CoinCount;
+
+            Destroy(collision.gameObject);
+        }
+
+        if(collision.gameObject.tag == "Water")
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
